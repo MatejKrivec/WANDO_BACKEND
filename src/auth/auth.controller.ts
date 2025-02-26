0ba639cb97ecf67f6a8fa964e4585d82490c2f31
MatejKrivec <matej.krivec@gmail.com>
2025-02-26 19:22:10 +0100

    Disabled the signup and confirm-signup endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,18 +5,18 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
+ /* @Post('signup')
   async signUp(@Body() body: { username: string, password: string, email: string }) {
     return await this.authService.signUp(body.username, body.password, body.email);
-  }
+  }*/
 
   @Post('signin')
   async signIn(@Body() body: { username: string, password: string }) {
     return await this.authService.signIn(body.username, body.password);
   }
 
-  @Post('confirm-signup')
+/*  @Post('confirm-signup')
     async confirmSignUp(@Body() body: { username: string, code: string }) {
     return await this.authService.confirmSignUp(body.username, body.code);
-    }
+    }*/
 }
